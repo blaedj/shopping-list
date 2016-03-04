d56c24cd6dee2bca0042efb570960115745a8148
blaedj <blaedj@gmail.com>
2016-03-04 07:57:28 -0600

    Add validation to new item view, Implement cancel
    The save button is only enabled if the description field is filled out.
    The cancel button now uses the proper view dismissal instead of relying
    on the 'error path' of the prepareForSegue method (i.e. the sender was
    not the save button)

--- shopping-list/AddGroceryListItemViewController.swift
+++ shopping-list/AddGroceryListItemViewController.swift
@@ -8,25 +8,53 @@
 
 import UIKit
 
-class AddGroceryListItemViewController: UIViewController {
+class AddGroceryListItemViewController: UIViewController, UITextFieldDelegate {
 
     // MARK: Properties
     var groceryItem: GroceryItem?
 
     @IBOutlet weak var saveButton: UIBarButtonItem!
-    @IBOutlet weak var itemDescription: UITextField!
+    @IBOutlet weak var itemDescriptionField: UITextField!
     @IBOutlet weak var quantityField: UITextField!
 
     // MARK: Navigation
+    @IBAction func cancel(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
     
     //this method lets you configure a view controller before it's presented
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
         if saveButton === sender {
-            let description = itemDescription.text ?? ""
+            let description = itemDescriptionField.text ?? ""
             let quantityText = quantityField.text ?? "0"
             let quantity = Int16(quantityText)
             print(description, quantity)
             groceryItem = GroceryItem(name: description, quantity: quantity)
         }
     }
+
+    // MARK: UITextFieldDelegate
+    func textFieldDidBeginEditing(textField: UITextField) {
+        //disable the save button while editing
+        saveButton.enabled = false
+        checkValidItemDescriptionField()
+    }
+
+    func textFieldDidEndEditing(textField: UITextField){
+        checkValidItemDescriptionField()
+        navigationItem.title = textField.text
+    }
+
+    func checkValidItemDescriptionField(){
+        let text = itemDescriptionField.text ?? ""
+        saveButton.enabled = !text.isEmpty
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        itemDescriptionField.delegate = self
+        checkValidItemDescriptionField()
+    }
+    
 }
